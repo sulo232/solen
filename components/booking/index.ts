e6e9443f3767e58d4896ecc24b60a27e797ba6d7
sulod <sulo232@icloud.com>
2026-04-01 23:40:12 +0200

    phase 1.1: add confirmation step with summary display

--- components/booking/index.ts
+++ components/booking/index.ts
@@ -2,6 +2,7 @@ export { default as ServiceSelectionStep } from './ServiceSelectionStep';
 export { default as StaffSelectionStep } from './StaffSelectionStep';
 export { default as DateSelectionStep } from './DateSelectionStep';
 export { default as TimeSelectionStep } from './TimeSelectionStep';
+export { default as ConfirmationStep } from './ConfirmationStep';
 export { default as ServiceCart } from './ServiceCart';
 export { default as StaffPicker } from './StaffPicker';
 export { default as GuestBookingForm } from './GuestBookingForm';
